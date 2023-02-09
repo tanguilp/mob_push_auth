6756e04df00efa966f6151b0250358c7cf489c83
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2023-02-09 20:28:54 +0300

    Configure staticity for custom js file

--- lib/mob_push_auth_web/endpoint.ex
+++ lib/mob_push_auth_web/endpoint.ex
@@ -25,7 +25,7 @@ defmodule MobPushAuthWeb.Endpoint do
     at: "/",
     from: :mob_push_auth,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images vendor favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
