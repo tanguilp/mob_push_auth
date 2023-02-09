52c7540e48be7aa2fd5351de754a24a80fd1e8f5
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2023-02-09 18:34:22 +0300

    Fix build script for render.com

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ MIX_ENV=prod mix compile
 # Compile assets
 npm install --prefix ./assets
 npm run deploy --prefix ./assets
-mix phx.digest
+mix assets.deploy
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
